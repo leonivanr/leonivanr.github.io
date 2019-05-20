5e9a0ce2cef98ab5469b3c22ceb3711f2030e529
Leon <leonivanrodrigo@gmail.com>
2019-05-19 21:49:56 -0300

    s

--- snake-game/app.js
+++ snake-game/app.js
@@ -1,3 +1,21 @@
+$("#gameCanvas").touchwipe({
+    wipeLeft: function () {
+        alert("left");
+    },
+    wipeRight: function () {
+        alert("right");
+    },
+    wipeUp: function () {
+        alert("up");
+    },
+    wipeDown: function () {
+        alert("down");
+    },
+    min_move_x: 20,
+    min_move_y: 20,
+    preventDefaultEvents: true
+});
+
 // UI Colors
 const canvasBorderColor = 'darkgreen';
 const canvasBackgroundColor = 'orange';
